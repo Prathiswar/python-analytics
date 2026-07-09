2d8f4cdcd8f5c9f965ae8d07943cca1426f23e87
Prathiswar Reddy <swanasambireddy@gmail.com>
2026-07-09 14:14:58 +0530

    Add files via upload

---
+++ Week-01/calculator.py
@@ -0,0 +1,15 @@
+def add(a, b):
+    return(a + b)
+def substract(a, b):
+    return(a - b)
+def multiply(a, b):
+    return(a * b)
+def divide(a, b):
+    return(a / b)
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Addition :", add(a, b))
+print("Subtraction :", substract(a, b))
+print("Multiplication :", multiply(a, b))
+print("Division :", divide(a, b))
+
---
+++ Week-01/morning_routine.py
@@ -0,0 +1,24 @@
+def wake_up():
+    print("Wake Up")
+    print("Brush teeth")
+    print("Drink Water")
+    print()
+
+
+def workout():
+    print("Go to Gym")
+    print("Train Hard")
+    print("Take Creatine")
+    print()
+
+
+
+def study():
+    print("Learn Python")
+    print("Practice Excel")
+    print("Think Like a Business Analyst")
+    print()
+
+wake_up()
+workout()
+study()
