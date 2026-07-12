349588eab9fbe3a42477c250464c0d8fac20c44a
Prathiswar Reddy <swanasambireddy@gmail.com>
2026-07-12 14:23:29 +0530

    Delete Week-01/student_profile_v2.py

--- Week-01/student_profile_v2.py
+++
@@ -1,7 +0,0 @@
-name = input("What is your name?")
-age = input("What is your age?")
-favourite_company = input("What is your favourite company?")
-
-print("Name:", name)
-print("Age:", age)
-print("Favourite Company:", favourite_company)
