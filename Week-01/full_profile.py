92bf17495888c6ec74772db8287bda8bfa3e489d
Prathiswar Reddy <swanasambireddy@gmail.com>
2026-07-12 14:24:33 +0530

    Delete Week-01/full_profile.py

--- Week-01/full_profile.py
+++
@@ -1,9 +0,0 @@
-Name = input("What is your name?")
-Age = input("What is your age?")
-College = input("What is your college name?")
-Dream_company = input("What is your dream company name?")
-
-print("Name:", Name)
-print("Age:", Age)
-print("College:", College)
-print("Dream Company:", Dream_company)
